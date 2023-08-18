a433edf570ad8c947d2980c2458e4c36bdcfbb33
Aarav-g123 <138991532+Aarav-g123@users.noreply.github.com>
2023-08-18 21:52:26 +0100

    Registers and test function added

---
+++ hassium/target/debug/deps/hassium-9adae17f095da529.d
@@ -0,0 +1,6 @@
+D:\dev\rust development\Register-Based-Rust-VM\hassium\target\debug\deps\hassium-9adae17f095da529.exe: src\main.rs src\vm.rs
+
+D:\dev\rust development\Register-Based-Rust-VM\hassium\target\debug\deps\hassium-9adae17f095da529.d: src\main.rs src\vm.rs
+
+src\main.rs:
+src\vm.rs:
