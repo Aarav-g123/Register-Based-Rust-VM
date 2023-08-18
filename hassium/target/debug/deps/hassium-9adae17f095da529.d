8b89907508cc251951528414b6a94605fc948cc8
Aarav-g123 <138991532+Aarav-g123@users.noreply.github.com>
2023-08-18 22:11:23 +0100

    Added instructions and the ability to decode opcodes

--- hassium/target/debug/deps/hassium-9adae17f095da529.d
+++ hassium/target/debug/deps/hassium-9adae17f095da529.d
@@ -1,6 +1,7 @@
-D:\dev\rust development\Register-Based-Rust-VM\hassium\target\debug\deps\hassium-9adae17f095da529.exe: src\main.rs src\vm.rs
+D:\dev\rust development\Register-Based-Rust-VM\hassium\target\debug\deps\hassium-9adae17f095da529.exe: src\main.rs src\vm.rs src\instructions.rs
 
-D:\dev\rust development\Register-Based-Rust-VM\hassium\target\debug\deps\hassium-9adae17f095da529.d: src\main.rs src\vm.rs
+D:\dev\rust development\Register-Based-Rust-VM\hassium\target\debug\deps\hassium-9adae17f095da529.d: src\main.rs src\vm.rs src\instructions.rs
 
 src\main.rs:
 src\vm.rs:
+src\instructions.rs:
